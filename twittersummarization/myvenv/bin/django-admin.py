4a9e5c9a67c5cc165ca1d3e1dc39bc33a890c35d
Adrian Hartarto p <adrianhp100797@gmail.com>
2018-12-30 12:55:40 +0700

    all

--- twittersummarization/myvenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/adrianhp97/Desktop/College/NLP/Text/tubes/twittersummarization/myvenv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
